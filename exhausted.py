a35e6628f8b318a0c3867f0a417fd804851055ce
Masataka Koike <plinecom@gmail.com>
2017-09-18 09:35:35 +0900

    psutil process test

--- exhausted.py
+++ exhausted.py
@@ -6,3 +6,12 @@ print psutil.cpu_percent(percpu=True)
 print psutil.virtual_memory()
 
 # 必要な物、ソケット通信、標準入力ハンドラ。実行状態調査。ファイルロック。
+
+
+for proc in psutil.process_iter():
+    try:
+        pinfo = proc.as_dict(attrs=['pid', 'name'])
+    except psutil.NoSuchProcess:
+        pass
+    else:
+        print(pinfo)
