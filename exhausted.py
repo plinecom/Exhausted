0f2bb0f22e8544519f9b9ea87f36facc5d510119
plinecom@cross-rendering.jp <dwnd8523>
2017-09-20 14:25:47 +0900

    date hostname

--- exhausted.py
+++ exhausted.py
@@ -1,21 +1,15 @@
 # -*- coding: utf-8 -*-
 import psutil
 import pymongo
+import os
+from datetime import datetime
+
 
 print psutil.cpu_percent(percpu=True)
 
 print psutil.virtual_memory()
 
 # 必要な物、ソケット通信、標準入力ハンドラ。実行状態調査。ファイルロック。
-
-
-for proc in psutil.process_iter():
-    try:
-        pinfo = proc.as_dict(attrs=['pid', 'name', 'cmdline', 'cpu_percent', 'num_threads', 'username', 'memory_percent'])
-    except psutil.NoSuchProcess:
-        pass
-    else:
-        print(pinfo)
 
 # mongodb へのアクセスを確立
 client = pymongo.MongoClient('127.0.0.1', 27017)
@@ -26,8 +20,22 @@ db = client.my_database
 # コレクションを作成 (名前: my_collection)
 co = db.my_collection
 
+date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+for proc in psutil.process_iter():
+    try:
+        pinfo = proc.as_dict(attrs=['pid', 'name', 'cmdline', 'cpu_percent', 'num_threads', 'username', 'memory_percent'])
+    except psutil.NoSuchProcess:
+        pass
+    else:
+        pinfo["hostname"] = os.uname()
+        pinfo["date"] = date
+        co.insert_one(pinfo)
+
+
+
 # なんか適当に保存
-co.insert_one({"test": 3})
+
 
 # 全部とってくる
 for data in co.find():
