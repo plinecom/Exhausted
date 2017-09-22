9f5fe890ab3eaf9e976f62b5e0cfec1ef04a88ec
plinecom@cross-rendering.jp <dwnd8523>
2017-09-22 12:53:21 +0900

    daemon_test

--- exhausted.py
+++ exhausted.py
@@ -4,6 +4,7 @@ import pymongo
 import os
 from datetime import datetime
 import time
+import sys
 
 # print psutil.cpu_percent(percpu=True)
 
@@ -11,29 +12,33 @@ import time
 
 # 必要な物、ソケット通信、標準入力ハンドラ。実行状態調査。ファイルロック。
 
-# mongodb へのアクセスを確立
-client = pymongo.MongoClient('127.0.0.1', 27017)
 
-# データベースを作成 (名前: my_database)
-db = client.my_database
+def data_collect():
 
-# コレクションを作成 (名前: my_collection)
-co = db.my_collection
+    # mongodb へのアクセスを確立
+    client = pymongo.MongoClient('127.0.0.1', 27017)
 
-while(True):
-    date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    # データベースを作成 (名前: my_database)
+    db = client.my_database
 
-    for proc in psutil.process_iter():
-        try:
-            pinfo = proc.as_dict(attrs=['pid', 'name', 'cmdline', 'environ', 'cpu_percent', 'num_threads', 'username', 'memory_percent'])
-        except psutil.NoSuchProcess:
-            pass
-        else:
-            pinfo["hostname"] = os.uname()
-            pinfo["date"] = date
-            co.insert_one(pinfo)
+    # コレクションを作成 (名前: my_collection)
+    co = db.my_collection
 
+    while True:
 
+        date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+        for proc in psutil.process_iter():
+            try:
+                pinfo = proc.as_dict(attrs=['pid', 'name', 'cmdline', 'environ', 'cpu_percent', 'num_threads', 'username', 'memory_percent'])
+            except psutil.NoSuchProcess:
+                pass
+            else:
+                pinfo["hostname"] = os.uname()
+                pinfo["date"] = date
+                co.insert_one(pinfo)
+
+        time.sleep(30)
 
 # なんか適当に保存
 
@@ -41,4 +46,19 @@ while(True):
 #    for data in co.find():
 #        print data
 
-    time.sleep(30)
+
+def fork():
+    pid = os.fork()
+
+    if pid > 0:
+        f = open('/var/run/exhaustedd.pid', 'w')
+        f.write(str(pid) + "\n")
+        f.close()
+        sys.exit()
+
+    if pid == 0:
+        data_collect()
+
+
+if __name__ == '__main__':
+    fork()
